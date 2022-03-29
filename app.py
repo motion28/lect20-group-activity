a4952301ad50ef5d9c9452f89d5904ec9991a14e
Moumin Ali <mouminiaali@Moumins-MacBook-Pro.local>
2022-03-29 16:29:12 -0400

    messed up code

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import os
 
 app = flask.Flask(__name__)
 
-MY_API_SECRET_KEY = "x95xd2dbxc3x175md5"
 
-BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
 
 params = {
     'q': 'braves', # Query keywords
